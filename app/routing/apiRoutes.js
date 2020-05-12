95622838277efbdaaed24edf5faa7d6d1cf7dbc4
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-12 12:54:24 -0600

    added post route and pseudocode

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,14 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
+
+    app.post("/api/friends", function (req, res) {
+        console.log (req.body)
+        res.json(req.body)
+        //logic to compare user that's coming in to every friend in friendsData array
+        // if else statement
+        //need to loop through friendsData
+    });
+
+
 }
