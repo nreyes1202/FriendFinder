efb97289c52a5d0f6a08066e0b0ce07bc4ae9e14
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-05-19 22:30:58 -0600

    added for loop to compare scores and console logged to see data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,29 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        console.log (req.body)
+        //console.log (req.body)
         res.json(req.body)
-        //logic to compare user that's coming in to every friend in friendsData array
-        // if else statement
-        //need to loop through friendsData
+
+        var i;
+
+        //Outer loop to access individual friends
+        for (i = 0; i < friendsData.length; i++) {
+            console.log(friendsData[i])
+            console.log(req.body)
+        //Inner loop to access friends' scores
+          //  friendsData[i];
+          //  friendsData[i].scores
+          var totalScore = 0;
+            for (j = 0; j < friendsData[i].scores.length; j++) {
+               totalScore += friendsData[i].scores[j] = req.body.scores[j]
+            }
+            console.log(totalScore)
+        };
+    
+
     });
 
 
 }
+
+//make a jquery ajax call to API route to get survery results from survey.html
