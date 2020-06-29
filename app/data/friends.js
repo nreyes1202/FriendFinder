f674785042ec5c1947cdec96452a1558b3ab1c31
Nilsen <58581719+nreyes1202@users.noreply.github.com>
2020-06-29 13:58:01 -0600

    added more compatibility questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,15 +19,15 @@ var friends = [
     "name":"Sally",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        5,
-        1,
         4,
-        4,
-        5,
         1,
+        3,
+        4,
         2,
+        2,
+        1,
         5,
-        4,
+        3,
         1
       ]
   },
@@ -36,14 +36,14 @@ var friends = [
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
-        1,
-        4,
-        4,
         5,
+        3,
+        5,
+        1,
         1,
         2,
+        3,
         5,
-        4,
         1
       ]
   }
